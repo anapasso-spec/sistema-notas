486c3f184a56fad34250a9211f37d2f40fcf6c46
Ana Ledier <ana.passo@portalsesisp.org.br>
2026-05-20 10:24:12 -0300

    feat: Adiciona notas e cálculo de média

--- main.cpp
+++ main.cpp
@@ -15,12 +15,20 @@ string nomes[20];
 
 int qAlunos;
 
+float notas[20][5];
+
+float media[20];
+
+int qDisciplinas;
+
 //Processamento
 
 cout << " --------- SISTEMA DE NOTAS v4.0 --------- " << endl;
 
 do 
+
 {
+
 cout << "Digite a quantidade de alunos ( sendo eles de 1 a 20 ): " << endl;
 
 cin >> qAlunos;
@@ -30,10 +38,52 @@ cin >> qAlunos;
 cin.ignore();
 
 for (int i = 0; i < qAlunos; i++)
+
 {
     cout << "Digite o nome do aluno " << i + 1 << ": " << endl;
 
     getline(cin, nomes[i]);
+}
+
+do
+
+{
+
+  cout << "\nDigite a quantidade de disciplinas ( 1 a 5 ): " << endl;
+  
+  cin >> qDisciplinas;
+
+} while (qDisciplinas < 1 || qDisciplinas > 5);
+
+cin.ignore();
+
+for (int i = 0; i < qAlunos; i++)
+
+{
+cout << "\nNotas de " << nomes[i] << ": " << endl;
+
+float soma = 0;
+
+for (int j = 0; j < qDisciplinas; j++)
+
+{
+
+do 
+
+{
+
+cout << "Disciplina " << j + 1 << " ( 1 a 5 ): ";
+
+cin >> notas[i][j];
+
+} while (notas[i][j] < 0 || notas[i][j] > 10 );
+
+soma += notas[i][j];
+
+}
+
+media[i] = soma / qDisciplinas;
+
 }
 
 //Saída
@@ -42,8 +92,8 @@ cout << "\nAlunos cadastrados: " << endl;
 
 for (int i = 0; i < qAlunos; i++)
 
-cout << "  " << i + 1 << ". " << nomes [i] << endl;
+cout << "  " << i + 1 << ". " << nomes[i] << endl;
 
     return 0;
-    
+
 }
